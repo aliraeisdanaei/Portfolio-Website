5d4151bb400fed640170de10dbd2ee6078aa777a
Ali Raeisdanaei <mrrookie2@gmail.com>
2020-11-03 14:34:03 -0500

    Adding more details to the content of website. Added a new project.

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -22,22 +22,29 @@ export class ProjectsComponent implements OnInit {
   projectsList: any=[{
     name: "Erika",
     languages: "C++",
-    description: "Complier, Lexer",
+    description: "Using C++, I am writing my very own programming language. The project includes a lexer that tokenizes the code. A shift reduce parser that creates an excecution tree. Finally, a generator produces LLVM IR code from the excecution tree.",
     link: "https://github.com/aliraeisdanaei/Erika",
     pic: "c++.svg"
 }, {
     name: "Linux Utilities",
     languages: "Bash",
-    description: "Bash scripts and a bunch of other stuff",
+    description: "Changing from Windows to a bare distro of Linux was not easy. I was determined to write a customizable computer experience. Using i3, I built many features and key bindings for programs. Through the process, I was able to understand the fundamental principles of Linux and gain deep insight into the legacy programs of Linux. Some of the basic utilities that I have written include, a punch timer, battery reminder, and photo converter.",
     link: "https://github.com/aliraeisdanaei/i3",
     pic: "linux.png"
 
 }, {
     name: "Portfolio Website",
     languages: "Angular, Typescript, HTML, CSS",
-    description: "Created portfolio website",
+    description: "This very website was coded from scratch using Angular. Technical and creative skills were required to put this through.",
     link: "https://github.com/aliraeisdanaei/Portfolio-Website",
     pic: "angular.png"
+},
+{
+  name: "Netflix Movie Suggester",
+  languages: "Python",
+  description: "Finding a movie to watch on Netflix can be hard. This program seeks to remedy that by scraping the web for all of the current movies running on Netflix and suggesting movies exhaustively based on your preferences. The text based user interface is complete with everything one would need to change their preferences to logging as someone else. ",
+  link: "https://github.com/aliraeisdanaei/NetflixMovieSuggester",
+  pic: "python.png"
 }];
 
 
